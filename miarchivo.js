bc052fd413875323448d4254444894214e5e8660
MarceloPedernera <87680340+MarceloPedernera@users.noreply.github.com>
2021-07-19 22:14:32 -0300

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,39 @@
+// Cotizar con funciones 
+
+function cotizar(){
+    let monto = prompt("Ingresar Monto");
+    alert("El monto ingresado es " + monto);
+} 
+
+    let cuota = prompt("Ingresar cantidad de cuotas");{
+    alert("La cantidad de cuotas ingresada es  " + cuota);
+}
+let cotizar = monto * cuota ;
+let iva = 1.21;   
+
+    //  Hacerlo Mediante IF 
+
+    if ( )
+    else {
+        (cotizar, iva)    
+    }
+
+let cotizacionFinal = cotizar (monto,cuota,iva); 
+
+console.log("El monto acreditado es" + cotizacionFinal); 
+
+// Otra Prueba 
+
+let monto = prompt("Ingresar Monto");
+let cuota = prompt("Ingresar cantidad de cuotas");
+
+const cotizacion  = function (a, b) { return a * b };
+
+const iva   = x => x * 0.21;
+
+let monto = 5000; 
+let cuota = 50; 
+
+let cuotaTotal = ((monto,cuota),iva); 
+
+console.log(cotizacionFinal);
