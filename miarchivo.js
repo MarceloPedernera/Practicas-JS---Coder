74a49956848b77606058afd9eddca594dcff2194
MarceloPedernera <87680340+MarceloPedernera@users.noreply.github.com>
2021-07-21 20:53:56 -0300

    Add files via upload
    Entrega Programacion Orientada a Objetos

--- miarchivo.js
+++ miarchivo.js
@@ -1,39 +1,62 @@
-// Cotizar con funciones 
 
-function cotizar(){
-    let monto = prompt("Ingresar Monto");
-    alert("El monto ingresado es " + monto);
-} 
-
-    let cuota = prompt("Ingresar cantidad de cuotas");{
-    alert("La cantidad de cuotas ingresada es  " + cuota);
+/* class Producto {
+    constructor(nombre, marca, precio) {
+        this.nombre  = nombre.toUpperCase();
+        this.marca = marca;
+        this.precio  = parseFloat(precio);
+        this.vendido = false;
+    }
+    sumarIva() {
+        this.precio = this.precio * 1.21;
+    }
+    vender() {
+        this.vendido = true;
+    }
 }
-let cotizar = monto * cuota ;
-let iva = 1.21;   
+const producto1 = new Producto("arroz", "Santa Marta", "125");
+const producto2 = new Producto("fideo", "50");
+producto1.sumarIva();
+producto1.vender();
+producto2.sumarIva();
+console.log(producto1);
+console.log(producto2);
 
-    //  Hacerlo Mediante IF 
+*/
 
-    if ( )
-    else {
-        (cotizar, iva)    
+// Form de registro a Cursos Online //
+
+class Usuario {
+    constructor(nombre, apellido, edad, ciudad, email) {
+
+        
+    // Conviene seguir usando This o pasar al nuevo metodo? 
+        this.nombre = nombre.toUpperCase();
+        this.apellido = apellido.toUpperCase();
+        this.edad =  parseFloat(edad);
+        this.ciudad = ciudad;
+        this.email = email;
+        this.registrado = false;
     }
 
-let cotizacionFinal = cotizar (monto,cuota,iva); 
+ // Esta Parte lo que quiero lograr es que el prompt aparezca al final y chequee si esta OK el mail y mas adelante enviar un mail de confirmacion
+    registrar() {
+        this.confirmarmail = prompt ("confirme su mail ");
+        
+        if (this.email === this.confirmarmail);
+        this.registrado = true;
+       
+    }
 
-console.log("El monto acreditado es" + cotizacionFinal); 
+}
+const Usuario1 = new Usuario("Marcelo", "Pedernera", "35", "Buenos Aires" , "ejemplo1@gmail.com");
+const Usuario2 = new Usuario("Sofia", "Rocha Jaje", "26", "Buenos Aires" ,"ejemplo2@outllok.com");
 
-// Otra Prueba 
+// Usuario1 "Marcelo" completaria y seria true, pero no Usuario2
 
-let monto = prompt("Ingresar Monto");
-let cuota = prompt("Ingresar cantidad de cuotas");
+//COMENTARIO PARA EL PROFESOR: Yo digo que Usuario1 Es true, pero como podria saber si completó todos los campos de forma automatica? 
 
-const cotizacion  = function (a, b) { return a * b };
+Usuario1.registrar() 
 
-const iva   = x => x * 0.21;
-
-let monto = 5000; 
-let cuota = 50; 
-
-let cuotaTotal = ((monto,cuota),iva); 
-
-console.log(cotizacionFinal);
+// Muestra por consola los Objetos del constructor "usuarios"
+console.log(Usuario1);
+console.log(Usuario2);
